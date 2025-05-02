7ff4017fc5b4a58e87b974b7f5da6716903e2c85
bflaggs <flaggsben@gmail.com>
2025-05-02 16:46:59 -0400

    Add what I have for bash script

---
+++ getLetyT3Requests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define input, output, and log paths
+input_dir="DEFINE_PATH_ON_FZ55"
+output_dir="DEFINE_PATH_ON_FZ55"
+log_file="DEFINE_PATH_ON_FZ55"
+
+# Loop over files in input_dir
+for input_file in "$input_dir/Ik_????_??_??_HHhMM.log"
+
+# Find input files
+
+# Check if output file already exists for this input file
+
+# Define output path and log path
+
+# Run python script
